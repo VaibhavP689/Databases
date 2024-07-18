3af946040f7c4f17b4b9d433b537731a7ceba637
VaibhavP689 <paranjivaibhav@gmail.com>
2024-07-18 11:03:44 -0700

    Demo Ready

--- static/script0.js
+++ static/script0.js
@@ -28,14 +28,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
             type: type,
             data: { labels: labels, datasets: datasets },
             options: {
-                //scales: {
-                    //yAxes: [{
-                        //id: 'y-axis-1',
-                        //type: 'linear',
-                        //position: 'left',
-                        //ticks: { beginAtZero: true }
-                    //}]
-                //},
+                plugins: {
+                    title: {
+                        display: true,
+                        text: containerId
+                    }, 
+                    padding: 4,
+                },
                 maintainAspectRatio: false
             }
         };
